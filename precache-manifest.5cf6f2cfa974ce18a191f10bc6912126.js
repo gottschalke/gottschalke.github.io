1782ae841193cdb72137b539a55ca24cd871a4e3
gottschalke <77029860+gottschalke@users.noreply.github.com>
2021-01-08 21:31:16 -0300

    Add files via upload

---
+++ precache-manifest.5cf6f2cfa974ce18a191f10bc6912126.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bcb6a9888383481ec7f916dd3cd46fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "788f537a04e184d9cfb0",
+    "url": "/static/css/main.02ee790b.chunk.css"
+  },
+  {
+    "revision": "2c297d934d389b5f2dbd",
+    "url": "/static/js/2.09960474.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.09960474.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "788f537a04e184d9cfb0",
+    "url": "/static/js/main.2d9e9c93.chunk.js"
+  },
+  {
+    "revision": "12b74a3bc71162ecc6b2",
+    "url": "/static/js/runtime-main.c8354511.js"
+  }
+]);
